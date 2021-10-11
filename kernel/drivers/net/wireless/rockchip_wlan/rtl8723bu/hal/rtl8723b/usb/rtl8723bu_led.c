e18218e8b26c73bb87ef4c70196f69e370c32e84
August <mingxin.android@gmail.com>
2021-10-11 22:26:44 +0800

    add rtl8821cu, rtl8812au, rtl8723bu driver, but not work well, rtk does not support these modues anymore, may be the drivers are old for android11

--- kernel/drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/rtl8723b/usb/rtl8723bu_led.c
+++ kernel/drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/rtl8723b/usb/rtl8723bu_led.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
- * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *                                        
+ * Copyright(c) 2007 - 2017 Realtek Corporation.
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
  * published by the Free Software Foundation.
@@ -11,86 +11,82 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- *
- *
- ******************************************************************************/
-//#include "drv_types.h"
+ *****************************************************************************/
+/* #include "drv_types.h" */
 #include "rtl8723b_hal.h"
+#ifdef CONFIG_RTW_SW_LED
 
-//================================================================================
-// LED object.
-//================================================================================
+/* ********************************************************************************
+ * LED object.
+ * ******************************************************************************** */
 
 
-//================================================================================
-//	Prototype of protected function.
-//================================================================================
+/* ********************************************************************************
+ *	Prototype of protected function.
+ * ******************************************************************************** */
 
-//================================================================================
-// LED_819xUsb routines. 
-//================================================================================
+/* ********************************************************************************
+ * LED_819xUsb routines.
+ * ******************************************************************************** */
 
-//
-//	Description:
-//		Turn on LED according to LedPin specified.
-//
+/*
+ *	Description:
+ *		Turn on LED according to LedPin specified.
+ *   */
 void
 SwLedOn_8723BU(
-	_adapter			*padapter, 
+	_adapter			*padapter,
 	PLED_USB		pLed
 )
 {
 	u8	LedCfg;
-	//HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter);
+	/* HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter); */
 
 	if (RTW_CANNOT_RUN(padapter))
 		return;
 
 	pLed->bLedOn = _TRUE;
-	
+
 }
 
 
-//
-//	Description:
-//		Turn off LED according to LedPin specified.
-//
+/*
+ *	Description:
+ *		Turn off LED according to LedPin specified.
+ *   */
 void
 SwLedOff_8723BU(
-	_adapter			*padapter, 
+	_adapter			*padapter,
 	PLED_USB		pLed
 )
 {
 	u8	LedCfg;
-	//HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter);
+	/* HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter); */
 
 	if (RTW_CANNOT_RUN(padapter))
 		goto exit;
 
 exit:
 	pLed->bLedOn = _FALSE;
-	
+
 }
 
-//================================================================================
-// Interface to manipulate LED objects.
-//================================================================================
+/* ********************************************************************************
+ * Interface to manipulate LED objects.
+ * ******************************************************************************** */
 
-//================================================================================
-// Default LED behavior.
-//================================================================================
+/* ********************************************************************************
+ * Default LED behavior.
+ * ******************************************************************************** */
 
-//
-//	Description:
-//		Initialize all LED_871x objects.
-//
+/*
+ *	Description:
+ *		Initialize all LED_871x objects.
+ *   */
 void
 rtl8723bu_InitSwLeds(
 	_adapter	*padapter
-	)
+)
 {
 	struct led_priv *pledpriv = &(padapter->ledpriv);
 
@@ -105,18 +101,18 @@ rtl8723bu_InitSwLeds(
 }
 
 
-//
-//	Description:
-//		DeInitialize all LED_819xUsb objects.
-//
+/*
+ *	Description:
+ *		DeInitialize all LED_819xUsb objects.
+ *   */
 void
 rtl8723bu_DeInitSwLeds(
 	_adapter	*padapter
-	)
+)
 {
 	struct led_priv	*ledpriv = &(padapter->ledpriv);
 
-	DeInitLed( &(ledpriv->SwLed0) );
-	DeInitLed( &(ledpriv->SwLed1) );
+	DeInitLed(&(ledpriv->SwLed0));
+	DeInitLed(&(ledpriv->SwLed1));
 }
-
+#endif
--- kernel/drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/rtl8723b/usb/rtl8723bu_recv.c
+++ kernel/drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/rtl8723b/usb/rtl8723bu_recv.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
- * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *                                        
+ * Copyright(c) 2007 - 2017 Realtek Corporation.
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
  * published by the Free Software Foundation.
@@ -11,13 +11,8 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- *
- *
- ******************************************************************************/
- #define _RTL8723BU_RECV_C_
+ *****************************************************************************/
+#define _RTL8723BU_RECV_C_
 
 #include <rtl8723b_hal.h>
 
@@ -30,4 +25,3 @@ void rtl8723bu_free_recv_priv(_adapter *padapter)
 {
 	usb_free_recv_priv(padapter, USB_INTR_CONTENT_LENGTH);
 }
-
